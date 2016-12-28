d60eb9b866e74109ee2578607e47eaa9a974c544
Ashish Kumar Sai <ashishkumarsai@me.com>
2016-12-28 12:55:44 +0530

    ADD: MapView and CoreDataModel entities

---
+++ Virtual Tourist/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  Virtual Tourist
+//
+//  Created by Ashutosh Kumar Sai on 28/12/16.
+//  Copyright © 2016 Ashish Rajendra Kumar Sai. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+
+}
+
