d8a554cf75c17c33845b9f1f3424b71d46ffaab5
Ashish Kumar Sai <ashishkumarsai@me.com>
2016-12-31 12:49:36 +0530

    MapView added to the next screen and can fetch the location of pin properly.

--- Virtual Tourist/imageViewController.swift
+++ Virtual Tourist/imageViewController.swift
@@ -10,14 +10,32 @@ import Foundation
 import UIKit
 import MapKit
 
-class imageViewController : UIViewController
+class imageViewController : UIViewController, MKMapViewDelegate
 {
      var passedCordinates: CLLocationCoordinate2D!
     
-    @IBOutlet weak var text: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         print("\(passedCordinates)")
-        text.text = "Check debugger"    }
+        let initialLocation = CLLocation(latitude: passedCordinates.latitude, longitude: passedCordinates.longitude)
+        mapView.delegate = self
+        centerMapOnLocation(location: initialLocation)
+        addpin()
+           }
     
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func addpin()
+    {
+        let pins = MKPointAnnotation()
+        let centerCoordinate = CLLocationCoordinate2D(latitude: passedCordinates.latitude, longitude:passedCordinates.longitude)
+        pins.coordinate = centerCoordinate
+        mapView.addAnnotation(pins)
+    }
 }
